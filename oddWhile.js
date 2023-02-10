7679df860736ad1d929e9db4149607493dd89daf
Issoufouiha <124552114+Issoufouiha@users.noreply.github.com>
2023-02-10 21:17:54 +0100

    Add files via upload

---
+++ oddWhile.js
@@ -0,0 +1,6 @@
+var i = 1;
+
+while (i <= 19) {
+  console.log(i);
+  i += 2;
+}
---
+++ weekendsVerified.js
@@ -0,0 +1,7 @@
+const day = prompt("Entrez un jour de la semaine :");
+
+if (day === "samedi" || day === "dimanche") {
+  console.log("weekend");
+} else {
+  console.log("weekday");
+}
