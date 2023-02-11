f4e598cc5cef72722368045b55bd8f4cc9fdbba3
Issoufouiha <124552114+Issoufouiha@users.noreply.github.com>
2023-02-11 21:35:43 +0100

    Create weekendsVerified.js

--- weekendsVerified.js
+++ weekendsVerified.js
@@ -1,7 +1,11 @@
 const day = prompt("Entrez un jour de la semaine :");
 
-if (day === "samedi" || day === "dimanche") {
-  console.log("weekend");
-} else {
-  console.log("weekday");
+let day = "jeudi";
+
+if (day === "samedi" || day === "dimanche")
+{
+    console.log("weekend");
+}
+else{
+    console.log("weekday");
 }
