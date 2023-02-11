cbb6b87454df6fc00c21ac5cd3122df163c787ab
Issoufouiha <124552114+Issoufouiha@users.noreply.github.com>
2023-02-11 11:08:40 +0100

    Update oddAndEven.js

--- oddAndEven.js
+++ oddAndEven.js
@@ -1,8 +1,7 @@
-for (var i = 1; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) {
     if (i % 2 === 0) {
-      console.log(i + " est pair");
+      console.log("paire");
     } else {
-      console.log(i + " est impair");
+      console.log("impaire");
     }
   }
-  
